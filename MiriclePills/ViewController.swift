3e28bd12b05817b0b0e7a2011650d15a667e2685
Omar Wilson <omar.wilson3@comcast.net>
2016-11-15 23:29:19 -0800

    half ui
    Added some labels and buttons including a state picker

--- MiriclePills/ViewController.swift
+++ MiriclePills/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Utah", "Colorado", "Washington", "New York", "Idaho", "Oregon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +28,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func stateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
